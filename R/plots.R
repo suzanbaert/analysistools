3b0b2130b9cc16a87a9f19424c73f3afdbc6939d
suzan baert <baert.s@pg.com>
2018-07-05 16:49:35 +0200

    ggplot_colors name change

--- R/plots.R
+++ R/plots.R
@@ -13,10 +13,10 @@
 #' @export
 #'
 #' @examples
-#' ggplot_color_hue(3)
+#' ggplot_colors(3)
 
 
-ggplot_color_hue <- function(n) {
+ggplot_colors <- function(n) {
   hues = seq(15, 375, length = n + 1)
   hcl(h = hues, l = 65, c = 100)[1:n]
 }
